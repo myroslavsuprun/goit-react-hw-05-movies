7ce3efb1a9ede4a3e0c377db8685e480eaafa695
MiroProger <sirmiroslavsuprun@gmail.com>
2022-11-08 23:41:56 +0200

    basename corrections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,12 +13,12 @@ export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
-        <Route path="movies/:movieId/" element={<MovieDetails />}>
+        <Route path="movies/:movieId" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
+        <Route index element={<Home />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
